4cc45c2ebea1b453a405e527304eae7124bcc502
Ilya Konyukhov <runsfor@gmail.com>
2019-08-07 12:02:02 +0300

    Add tests for log.init()

---
+++ test/app-tap/logger_init.test.lua
@@ -0,0 +1,31 @@
+#!/usr/bin/env tarantool
+
+local test = require('tap').test('log')
+test:plan(3)
+
+local filename = "1.log"
+local message = "Hello, World!"
+
+local log = require('log')
+log.init({init_str = filename})
+
+local file = io.open(filename)
+
+while file:read() do
+end
+
+log.info(message)
+test:is(file:read():match('I>%s+(.*)'), message, 'logging works without box.cfg{}')
+
+log.verbose(message)
+test:isnil(file:read(), 'Default log level is 5 (INFO)')
+
+log.level(6)
+
+message = 'updates log level works'
+log.verbose(message)
+test:is(file:read():match('V>%s+(.*)'), message, 'verbose logging')
+
+file:close()
+test:check()
+os.exit()
